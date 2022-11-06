4237aa8e6870dd34e7a94466f96b55bd07ca8050
Tigran Manasyan <manasyan_tigran@mail.ru>
2022-11-07 01:28:16 +0700

    add first version of cosine topocorrection

--- processing_algorithm.py
+++ processing_algorithm.py
@@ -1,3 +1,5 @@
+from math import cos, radians
+
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,
                        QgsProcessingAlgorithm,
@@ -31,13 +33,13 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
         """
         Returns the unique algorithm name.
         """
-        return 'tigran_luminance'
+        return 'luminance_map'
 
     def displayName(self):
         """
         Returns the translated algorithm name.
         """
-        return self.tr('Tigran luminance')
+        return self.tr('Luminance map')
 
     def group(self):
         """
@@ -50,13 +52,13 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
         Returns the unique ID of the group this algorithm belongs
         to.
         """
-        return 'tigran_luminance'
+        return 'tigran'
 
     def shortHelpString(self):
         """
         Returns a localised short help string for the algorithm.
         """
-        return self.tr('Example algorithm short description')
+        return self.tr('Builds luminance map')
 
     def initAlgorithm(self, config=None):
         """
@@ -80,32 +82,16 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
         )
 
         self.addParameter(
-            QgsProcessingParameterRasterLayer(
-                'SLOPE',
-                self.tr('Input slope layer'),
-                optional=True
-            )
-        )
-
-        self.addParameter(
-            QgsProcessingParameterRasterLayer(
-                'ASPECT',
-                self.tr('Input aspect layer'),
-                optional=True
-            )
-        )
-
-        self.addParameter(
             QgsProcessingParameterNumber(
                 'SZA',
-                self.tr('Input aspect layer')
+                self.tr('Solar zenith angle')
             )
         )
 
         self.addParameter(
             QgsProcessingParameterNumber(
                 'SOLAR_AZIMUTH',
-                self.tr('Solar Azimuth')
+                self.tr('Solar azimuth')
             )
         )
 
@@ -137,7 +123,50 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        return self.compute_luminance(parameters, context, slope_layer, aspect_layer)
+        sza = self.parameterAsDouble(parameters,
+                                     'SZA',
+                                     context)
+
+        sza_radians = radians(sza)
+
+        luminance = self.compute_luminance(parameters, context, slope_layer, aspect_layer)
+
+        return self.cosine_topocorrection(parameters, luminance, input_layer, sza_radians)
+
+    def cosine_topocorrection(self, parameters, luminance_layer, input_layer, sza_radians):
+        sza_cosine = cos(sza_radians)
+
+        result_bands = []
+        for band_id in range(input_layer.bandCount()):
+            results = processing.runAndLoadResults(
+                'gdal:rastercalculator',
+                {
+                    # create layer from luminance_layer
+                    'INPUT_A': luminance_layer,
+                    'BAND_A': 1,
+                    'INPUT_B': input_layer,
+                    'BAND_B': band_id + 1,
+                    'FORMULA': f'((B*{sza_cosine})/cos(A))',
+                    'OUTPUT': 'TEMPORARY_OUTPUT'
+                })
+            # тут как я понял мы можем убрать dataProvider().dataSourceUri(), т.к.
+            # в аутпуте пхду лежит строка
+            result_bands.append(results['OUTPUT'].dataProvider().dataSourceUri())
+
+        #     todo merge bands into layer
+        return processing.run(
+            "gdal:merge",
+            {
+                'INPUT': result_bands,
+                'PCT': False,
+                'SEPARATE': True,
+                'NODATA_INPUT': None,
+                'NODATA_OUTPUT': None,
+                'OPTIONS': '',
+                'EXTRA': '',
+                'DATA_TYPE': 5,
+                'OUTPUT':  parameters['OUTPUT']
+            })
 
     def build_slope_layer(self, dem_layer):
         results = processing.run(
@@ -175,15 +204,17 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
         return results['OUTPUT']
 
     def compute_luminance(self, parameters, context, slope_layer, aspect_layer):
+        # todo
         sza = self.parameterAsDouble(parameters,
                                      'SZA',
                                      context)
+        sza_radians = radians(sza)
 
         solar_azimuth = self.parameterAsDouble(parameters,
                                                'SOLAR_AZIMUTH',
                                                context)
+        solar_azimuth_radians = radians(solar_azimuth)
 
-        print(f'(cos({sza})*cos(A) + sin({sza})*sin(A)*cos(B - {solar_azimuth}))')
         results = processing.runAndLoadResults(
             'gdal:rastercalculator',
             {
@@ -191,7 +222,8 @@ class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
                 'BAND_A': 1,
                 'INPUT_B': aspect_layer,
                 'BAND_B': 1,
-                'FORMULA': f'(cos({sza})*cos(A) + sin({sza})*sin(A)*cos(B - {solar_azimuth}))',
+                'FORMULA': f'(cos({sza_radians})*cos(deg2rad(A)) + '
+                           f'sin({sza_radians})*sin(deg2rad(A))*cos(deg2rad(B) - {solar_azimuth_radians}))',
                 'OUTPUT': parameters['OUTPUT']
             })
 
